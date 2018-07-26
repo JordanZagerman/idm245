39dc4a8fdd67b0dda668cf4ae3519c8176ec5fc8
Jordan Zagerman <jordan.zagerman@drexel.edu>
2018-07-26 15:12:36 -0400

    almost got all the text working

--- js/example.js
+++ js/example.js
@@ -1,57 +1,72 @@
 window.onload = function () {
 
-    var game = new Phaser.Game(720, 960, Phaser.AUTO, 'phaser-canvas', {
-      preload: preload,
-      create: create
-    });
+  var game = new Phaser.Game(720, 960, Phaser.AUTO, 'phaser-canvas', {
+    preload: preload,
+    create: create
+  });
 
-    // Images must be preloaded
-    function preload() {
-      game.load.image('background', '../img/start_background.png');
-      game.load.image('marker', '../img/marker.png');
-      // Images is 1089 pixels wide divided up into 363 pixel wide chunks
-      game.load.spritesheet('playButton', '../img/proceed_button.png', 363, 120);
-      game.load.spritesheet('helpButton', '../img/help_button.png', 177, 100);
-      
-    }
+  // Images must be preloaded
+  function preload() {
+    game.load.image('background', '../img/start_background.png');
+    game.load.image('marker', '../img/marker.png');
+    // Images is 1089 pixels wide divided up into 363 pixel wide chunks
+    game.load.spritesheet('playButton', '../img/proceed_button.png', 363, 120);
+    game.load.spritesheet('helpButton', '../img/help_button.png', 177, 100);
+    this.game.load.bitmapFont('Krungthep', 'assets/font/font.png', 'assets/font/font.fnt');
 
-    // Text doesn't need to be preloaded
-    function create() {
-      var spBackground = game.add.sprite(0, 0, 'background');
+  }
 
-      //game.world.centerX/Y is an equation that automatically does the anchor point centering equations
-      var spMarker = game.add.sprite(game.world.centerX, game.world.centerY, 'marker');
-      // top left is 0,0 bottom right is 1,1
-      spMarker.anchor.setTo(0.5, 0.5);
+  // Text doesn't need to be preloaded
+  function create() {
+    var spBackground = game.add.sprite(0, 0, 'background');
 
-      var scoreNum = 1200;
+    //game.world.centerX/Y is an equation that automatically does the anchor point centering equations
+    var spMarker = game.add.sprite(game.world.centerX, game.world.centerY, 'marker');
+    // top left is 0,0 bottom right is 1,1
+    spMarker.anchor.setTo(0.5, 0.5);
 
-      // Text Style
-      var generalStyle = {
-        width: '150px',
-        font: '35px Arial',
-        fill: 'black',
-        allign: 'left'
-      };
+    var scoreNum = 1200;
 
-      var textScore = game.add.text(25, 15, scoreNum, generalStyle);
+    // Text Style
+    var generalStyle = {
+      // width: '200px',
+      // font: '50px Krungthep',
+      // fill: 'black',
+      // allign: 'left'
+    };
 
-      // Add button
-      // The number given are the indexes of the frames in this order: OVER, OUT, DOWN
-      // bt short for button
-      var btPlay = game.add.button(game.world.centerX + 1, game.world.centerY + 47 , 'playButton', actionOnClick, this, 1, 0, 2);
-      btPlay.anchor.setTo(0.5, 0.5);
-      
-      // Add button
-      // The number given are the indexes of the frames in this order: OVER, OUT, DOWN
-      // bt short for button
-      var btHelp = game.add.button(game.world.centerX + 1, game.world.centerY + 303 , 'helpButton', actionOnClick, this, 1, 0, 2);
-      btHelp.anchor.setTo(0.5, 0.5);
-    } // end of create function
+    var textScore = game.add.text(125, 205, scoreNum);
+
+    //	Center align
+    textScore.anchor.set(0.5);
+    textScore.align = 'center';
+
+    //	Font style
+    textScore.font = 'KrungThep';
+    textScore.fontSize = 80;
+    // textScore.fontWeight = 'bold';
+
+    // //	Stroke color and thickness
+    // textScore.stroke = '#000000';
+    // textScore.strokeThickness = 6;
+    textScore.fill = '#FFFFFF';
+
+    // Add button
+    // The number given are the indexes of the frames in this order: OVER, OUT, DOWN
+    // bt short for button
+    var btPlay = game.add.button(game.world.centerX + 1, game.world.centerY + 47, 'playButton', actionOnClick, this, 1, 0, 2);
+    btPlay.anchor.setTo(0.5, 0.5);
+
+    // Add button
+    // The number given are the indexes of the frames in this order: OVER, OUT, DOWN
+    // bt short for button
+    var btHelp = game.add.button(game.world.centerX + 1, game.world.centerY + 303, 'helpButton', actionOnClick, this, 1, 0, 2);
+    btHelp.anchor.setTo(0.5, 0.5);
+  } // end of create function
 
 
-    function actionOnClick () {
-      alert('Play button clicked');
-    }
+  function actionOnClick() {
+    alert('Play button clicked');
+  }
 
-  } // end onload function
+} // end onload function
--- js/game_screen.js
+++ js/game_screen.js
@@ -1,39 +1,85 @@
 window.onload = function () {
 
-    var game = new Phaser.Game(720, 960, Phaser.AUTO, 'phaser-canvas', {
-      preload: preload,
-      create: create
-    });
+  var game = new Phaser.Game(720, 960, Phaser.AUTO, 'phaser-canvas', {
+    preload: preload,
+    create: create
+  });
 
-    // Images must be preloaded
-    function preload() {
-      game.load.image('background', '../img/0_score.png');
-      
+  // Images must be preloaded
+  function preload() {
+    game.load.image('background', '../img/0_score.png');
+    this.game.load.bitmapFont('Krungthep', 'assets/font/font.png', 'assets/font/font.fnt');
+  }
+
+  // Text doesn't need to be preloaded
+  function create() {
+    game.stage.backgroundColor = "#680000";
+    var spBackground = game.add.sprite(0, 0, 'background');
+
+    //game.world.centerX/Y is an equation that automatically does the anchor point centering equations
+    var scoreNum = 0;
+
+    // Text Style
+    var generalStyle = {
+      // width: '200px',
+      // font: '50px Krungthep',
+      // fill: 'black',
+      // allign: 'left'
     }
 
-    // Text doesn't need to be preloaded
-    function create() {
-      game.stage.backgroundColor = "#680000";
-      var spBackground = game.add.sprite(0, 0, 'background');
+    var textScore = game.add.text(39, 36, scoreNum);
 
-      //game.world.centerX/Y is an equation that automatically does the anchor point centering equations
+    //	Center align
+    textScore.anchor.set(0.5);
+    textScore.align = 'center';
 
-      // var scoreNum = 1200;
+    //	Font style
+    textScore.font = 'KrungThep';
+    textScore.fontSize = 72;
+    // textScore.fontWeight = 'bold';
 
-      // // Text Style
-      // var generalStyle = {
-      //   width: '150px',
-      //   font: '35px Arial',
-      //   fill: 'white',
-      //   allign: 'left'
-      // };
+    // //	Stroke color and thickness
+    // textScore.stroke = '#000000';
+    // textScore.strokeThickness = 6;
+    textScore.fill = '#000000';
 
-      // var textScore = game.add.text(25, 15, scoreNum, generalStyle);
-    } // end of create function
+    // TIMER SECTION
 
+    // var textTimer = game.add.text(25, 15, timerNumMinutes, generalStyle);
+    var timerNumMinutes = 0;
 
-    function actionOnClick () {
-      alert('Play button clicked');
+    // Text Style
+    var generalStyle = {
+      // width: '200px',
+      // font: '50px Krungthep',
+      // fill: 'black',
+      // allign: 'left'
     }
 
-  } // end onload function
+    var textTimer = game.add.text(572, 40, timerNumMinutes);
+
+    //	Center align
+    textTimer.anchor.set(0.5);
+    textTimer.align = 'center';
+
+    //	Font style
+    textTimer.font = 'KrungThep';
+    textTimer.fontSize = 72;
+    // textTimer.fontWeight = 'bold';
+
+    // //	Stroke color and thickness
+    // textTimer.stroke = '#000000';
+    // textTimer.strokeThickness = 6;
+    textTimer.fill = '#000000';
+
+
+    // var textTimer = game.add.text(25, 15, timerNum, generalStyle);
+
+  } // end of create function
+
+
+  function actionOnClick() {
+    alert('Play button clicked');
+  }
+
+} // end onload function
