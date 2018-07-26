f18ef7cba11aab39fb5842344f29daf208b99b5c
Jordan Zagerman <jordan.zagerman@drexel.edu>
2018-07-26 17:53:52 -0400

    text works on game screen, added help files

--- js/game_screen.js
+++ js/game_screen.js
@@ -14,7 +14,6 @@ window.onload = function () {
   // Text doesn't need to be preloaded
   function create() {
     game.stage.backgroundColor = "#680000";
-    var spBackground = game.add.sprite(0, 0, 'background');
 
     //game.world.centerX/Y is an equation that automatically does the anchor point centering equations
     var scoreNum = 0;
@@ -27,7 +26,7 @@ window.onload = function () {
       // allign: 'left'
     }
 
-    var textScore = game.add.text(39, 36, scoreNum);
+    var textScore = game.add.text(39, 40, scoreNum);
 
     //	Center align
     textScore.anchor.set(0.5);
@@ -39,14 +38,14 @@ window.onload = function () {
     // textScore.fontWeight = 'bold';
 
     // //	Stroke color and thickness
-    // textScore.stroke = '#000000';
+    // textScore.stroke = '#FFFFFF';
     // textScore.strokeThickness = 6;
-    textScore.fill = '#000000';
+    textScore.fill = '#FFFFFF';
 
     // TIMER SECTION
 
     // var textTimer = game.add.text(25, 15, timerNumMinutes, generalStyle);
-    var timerNumMinutes = 0;
+    var timerNumMinutes = '00';
 
     // Text Style
     var generalStyle = {
@@ -56,24 +55,83 @@ window.onload = function () {
       // allign: 'left'
     }
 
-    var textTimer = game.add.text(572, 40, timerNumMinutes);
+    var textTimerMinutes = game.add.text(548, 40, timerNumMinutes);
 
     //	Center align
-    textTimer.anchor.set(0.5);
-    textTimer.align = 'center';
+    textTimerMinutes.anchor.set(0.5);
+    textTimerMinutes.align = 'center';
 
     //	Font style
-    textTimer.font = 'KrungThep';
-    textTimer.fontSize = 72;
-    // textTimer.fontWeight = 'bold';
+    textTimerMinutes.font = 'KrungThep';
+    textTimerMinutes.fontSize = 72;
+    // textTimerMinutes.fontWeight = 'bold';
 
     // //	Stroke color and thickness
-    // textTimer.stroke = '#000000';
-    // textTimer.strokeThickness = 6;
-    textTimer.fill = '#000000';
+    // textTimerMinutes.stroke = '#FFFFFF';
+    // textTimerMinutes.strokeThickness = 6;
+    textTimerMinutes.fill = '#FFFFFF';
 
 
-    // var textTimer = game.add.text(25, 15, timerNum, generalStyle);
+    // var textTimerMinutes = game.add.text(25, 15, timerNum, generalStyle);
+
+    // var textTimer = game.add.text(25, 15, timerNumMinutes, generalStyle);
+    var timerNumSeconds = '00';
+
+    // Text Style
+    var generalStyle = {
+      // width: '200px',
+      // font: '50px Krungthep',
+      // fill: 'black',
+      // allign: 'left'
+    }
+
+    var textTimerSeconds = game.add.text(668, 40, timerNumSeconds);
+
+    //	Center align
+    textTimerSeconds.anchor.set(0.5);
+    textTimerSeconds.align = 'center';
+
+    //	Font style
+    textTimerSeconds.font = 'KrungThep';
+    textTimerSeconds.fontSize = 72;
+    // textTimerSeconds.fontWeight = 'bold';
+
+    // //	Stroke color and thickness
+    // textTimerSeconds.stroke = '#FFFFFF';
+    // textTimerSeconds.strokeThickness = 6;
+    textTimerSeconds.fill = '#FFFFFF';
+
+
+    // var textTimerSeconds = game.add.text(25, 15, timerNum, generalStyle);
+
+    var timerNumColon = ':';
+
+    // Text Style
+    var generalStyle = {
+      // width: '200px',
+      // font: '50px Krungthep',
+      // fill: 'black',
+      // allign: 'left'
+    }
+
+    const textTimerColon = game.add.text(608, 40, timerNumColon);
+
+    //	Center align
+    textTimerColon.anchor.set(0.5);
+    textTimerColon.align = 'center';
+
+    //	Font style
+    textTimerColon.font = 'KrungThep';
+    textTimerColon.fontSize = 72;
+    // textTimerColon.fontWeight = 'bold';
+
+    // //	Stroke color and thickness
+    // textTimerColon.stroke = '#FFFFFF';
+    // textTimerColon.strokeThickness = 6;
+    textTimerColon.fill = '#FFFFFF';
+
+
+    // var textTimerColon = game.add.text(25, 15, timerNum, generalStyle);
 
   } // end of create function
 
